fad2f9bde0e689a678caeefc71bedcd2ba68f25e
Ermias Wakgari Yadeta <ermiaswakgarisse5@gmail.com>
2025-07-04 13:51:47 +0300

    changed backend to deployed

--- Client/src/axiosConfig.tsx
+++ Client/src/axiosConfig.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
-  // baseURL: 'https://blog-e0l8.onrender.com/api/v1',
+  baseURL: 'https://blog-e0l8.onrender.com/api/v1',
 })
 instance.interceptors.request.use(
   (config) => {
