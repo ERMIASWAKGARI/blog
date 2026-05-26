7cab6b2d7cfb2c50b34a70e646fafa815898499f
Ermias Wakgari <ermiaswakgarisse5@gmail.com>
2026-05-26 10:37:09 +0300

    fixed bug

--- Client/src/axiosConfig.tsx
+++ Client/src/axiosConfig.tsx
@@ -1,7 +1,10 @@
 import axios from "axios";
+
+// take baseURL from .env file
 const instance = axios.create({
   // baseURL: "https://blog-e0l8.onrender.com/api/v1",
-  baseURL: "http://localhost:8000/api/v1",
+  // use Vite's import.meta.env instead of process.env to avoid TS "Cannot find name 'process'" errors
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
 });
 instance.interceptors.request.use(
   (config) => {
@@ -13,6 +16,6 @@ instance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 export default instance;
